df837739f89225347d10a99d793720b21ccaf546
Vanh1200 <axitpicric@gmail.com>
2019-01-19 09:58:16 +0700

    create splash screen

--- app/src/main/java/com/vanh1200/lovemusic/screen/main/MainActivity.java
+++ app/src/main/java/com/vanh1200/lovemusic/screen/main/MainActivity.java
@@ -1,7 +1,11 @@
-package com.vanh1200.lovemusic;
+package com.vanh1200.lovemusic.screen.main;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.vanh1200.lovemusic.R;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public static Intent getIntent(Context context) {
+        return new Intent(context, MainActivity.class);
     }
 }
