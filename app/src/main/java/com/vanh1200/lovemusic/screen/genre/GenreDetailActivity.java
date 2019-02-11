5c4237ae350dd9499415d6f0db77857526ec5974
Vanh1200 <axitpicric@gmail.com>
2019-02-11 15:46:35 +0700

    create layout for detail screen

---
+++ app/src/main/java/com/vanh1200/lovemusic/screen/genre/GenreDetailActivity.java
@@ -0,0 +1,19 @@
+package com.vanh1200.lovemusic.screen.genre;
+
+import android.os.Bundle;
+
+import com.vanh1200.lovemusic.R;
+import com.vanh1200.lovemusic.base.BaseActivity;
+
+public class GenreDetailActivity extends BaseActivity {
+
+    @Override
+    protected int getLayoutResource() {
+        return R.layout.activity_genre_detail;
+    }
+
+    @Override
+    protected void initViews(Bundle saveInstanceState) {
+
+    }
+}
